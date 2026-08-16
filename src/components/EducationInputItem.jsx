a109550d8432d8a63f0e150faede9c1b36adb546
d1scipline <143661704+d1scipline@users.noreply.github.com>
2026-08-17 01:49:25 +0300

    feat: style the education section layout in the editor

--- src/components/EducationInputItem.jsx
+++ src/components/EducationInputItem.jsx
@@ -1,3 +1,5 @@
+import { Trash2 } from "lucide-react";
+
 export default function EducationInputItem({
   data,
   removeEducation,
@@ -5,31 +7,57 @@ export default function EducationInputItem({
 }) {
   return (
     <div className="education-input-item">
-      <input
-        placeholder="Degree"
-        onChange={(e) => {
-          updateEducation(data.id, "degree", e.target.value);
-        }}
-      ></input>
-      <input
-        placeholder="University"
-        onChange={(e) => {
-          updateEducation(data.id, "university", e.target.value);
-        }}
-      ></input>
-      <input
-        type="date"
-        onChange={(e) => {
-          updateEducation(data.id, "startDate", e.target.value);
-        }}
-      ></input>
-      <input
-        type="date"
-        onChange={(e) => {
-          updateEducation(data.id, "endDate", e.target.value);
-        }}
-      ></input>
-      <button onClick={() => removeEducation(data.id)}>Remove</button>
+      <div className="education-top-row">
+        <div className="degree-editor-item">
+          <label>Degree</label>
+          <input
+            placeholder="Degree"
+            onChange={(e) => {
+              updateEducation(data.id, "degree", e.target.value);
+            }}
+          ></input>
+        </div>
+
+        <div className="university-editor-item">
+          <label>University</label>
+          <input
+            placeholder="University"
+            onChange={(e) => {
+              updateEducation(data.id, "university", e.target.value);
+            }}
+          ></input>
+        </div>
+      </div>
+
+      <div className="education-bottom-row">
+        <div className="education-start-date-item">
+          <label>Start Date</label>
+          <input
+            type="month"
+            onChange={(e) => {
+              updateEducation(data.id, "startDate", e.target.value);
+            }}
+          ></input>
+        </div>
+
+        <div className="education-end-date-item">
+          <label>End Date</label>
+          <input
+            type="month"
+            onChange={(e) => {
+              updateEducation(data.id, "endDate", e.target.value);
+            }}
+          ></input>
+        </div>
+      </div>
+
+      <button
+        className="delete-button delete-education"
+        onClick={() => removeEducation(data.id)}
+      >
+        <Trash2></Trash2>
+        Delete Education
+      </button>
     </div>
   );
 }
